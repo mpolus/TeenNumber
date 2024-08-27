f2c516059b570cb7db714e653f7852c7996f99f1
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-27 13:17:50 -0400

    teen

--- src/TeenNumberChecker.java
+++ src/TeenNumberChecker.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class TeenNumberChecker {
+public class TeenNumberChecker {
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+
+    private static boolean hasTeen(int num1, int num2, int num3) {
+        if (num1 >= 13 && num1 <= 19){
+            return true;
+        }
+        if (num2 >= 13 && num2 <= 19){
+            return true;
+        }
+        if (num3 >= 13 && num3 <= 19){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    private static boolean isTeen(int num4){
+        if (num4 >= 13 && num4 <= 19){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
